d740b101a435725f276e7057d83efc6f389a86c6
JeanRojasIfms <jean.rojas@ifms.edu.br>
2022-11-03 14:48:46 -0400

    alteracao no arquivo de conexao

--- config/configuracao.php
+++ config/configuracao.php
@@ -1,5 +1,5 @@
 <?php
-    $mysql = new mysqli('localhost','root','','carro');
+    $mysql = new mysqli('us-cdbr-east-06.cleardb.net','b98f7a13b4d42b','828cf788','heroku_8843d2e39002498');
     $mysql->set_charset('utf8');
     if ($mysql == false)
         echo "Falha na conexão do bd!";
